ccee57f7c68ac3e78489a0635f4f2678daf1fc68
Maxi <mpezzotta@folcode.com>
2023-01-22 10:56:52 -0300

    custom style for anchor element

--- components/modules/experience/components/contentPanel/contentPanel.component.tsx
+++ components/modules/experience/components/contentPanel/contentPanel.component.tsx
@@ -1,4 +1,6 @@
+import { CSSProperties } from 'react'
 import { motion } from 'framer-motion'
+import { useTheme } from '@mui/material'
 import { MOTION_PROPS } from 'constants/motion'
 import styles from './contentPanel.module.css'
 
@@ -9,24 +11,28 @@ type Props = {
   actions: { id: number; description: string }[]
 }
 
-export const ContentPanel = ({ title, actions, company, url }: Props) => (
-  <div className={styles.container}>
-    <motion.p className={styles.title}>
-      <a href={url} target="_blank">
-        {company}
-      </a>
-      {title}
-    </motion.p>
-    <ul className={styles.actions}>
-      {actions.map(({ description, id }) => (
-        <motion.li
-          key={id}
-          className={styles.items}
-          {...MOTION_PROPS}
-          transition={{ duration: 0.4 + id }}>
-          {description}
-        </motion.li>
-      ))}
-    </ul>
-  </div>
-)
+export const ContentPanel = ({ title, actions, company, url }: Props) => {
+  const { palette } = useTheme()
+  const customStyles = { '--anchorColor': palette.primary.main } as CSSProperties
+  return (
+    <div className={styles.container}>
+      <motion.p className={styles.title}>
+        <a href={url} target="_blank" style={customStyles}>
+          {company}
+        </a>
+        {title}
+      </motion.p>
+      <ul className={styles.actions}>
+        {actions.map(({ description, id }) => (
+          <motion.li
+            key={id}
+            className={styles.items}
+            {...MOTION_PROPS}
+            transition={{ duration: 0.4 + id }}>
+            {description}
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  )
+}
