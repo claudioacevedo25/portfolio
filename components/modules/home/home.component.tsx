d6abc3c7aeee3cfca3bd86b7878555d98f8038f3
Maxi <mpezzotta@folcode.com>
2023-01-23 12:14:32 -0300

    change text color && remove console warning

--- components/modules/home/home.component.tsx
+++ components/modules/home/home.component.tsx
@@ -1,26 +1,29 @@
-import { Typography } from '@mui/material'
+import { useTheme } from '@mui/material'
 import { motion } from 'framer-motion'
 import { MOTION_PROPS } from 'constants/motion'
 import styles from './home.module.css'
 
-export const Home = () => (
-  <section className={styles.container}>
-    <motion.p {...MOTION_PROPS}>
-      <Typography color="primary">Hi, my name is</Typography>
-    </motion.p>
-    <motion.h1 className={styles.title} {...MOTION_PROPS} transition={{ duration: 0.6 }}>
-      <span />
-      <span className={styles.name}>Maximiliano</span>
-      <span className={styles.lastName}>Pezzotta.</span>
-    </motion.h1>
+export const Home = () => {
+  const { palette } = useTheme()
+  return (
+    <section className={styles.container}>
+      <motion.p {...MOTION_PROPS} style={{ color: palette.primary.main }}>
+        Hi, my name is
+      </motion.p>
+      <motion.h1 className={styles.title} {...MOTION_PROPS} transition={{ duration: 0.6 }}>
+        <span />
+        <span className={styles.name}>Maximiliano</span>
+        <span className={styles.lastName}>Pezzotta.</span>
+      </motion.h1>
 
-    <motion.h2 className={styles.subtitle} {...MOTION_PROPS} transition={{ duration: 0.65 }}>
-      I build digital solutions for the web.
-    </motion.h2>
+      <motion.h2 className={styles.subtitle} {...MOTION_PROPS} transition={{ duration: 0.65 }}>
+        I build digital solutions for the web.
+      </motion.h2>
 
-    <motion.p className={styles.description} {...MOTION_PROPS} transition={{ duration: 0.7 }}>
-      I’m a software engineer (specializing Frontend) passionate in building exceptional digital
-      experiences. Currently, I’m focused on building accessible, human-centered products.
-    </motion.p>
-  </section>
-)
+      <motion.p className={styles.description} {...MOTION_PROPS} transition={{ duration: 0.7 }}>
+        I’m a software engineer (specializing Frontend) passionate in building exceptional digital
+        experiences. Currently, I’m focused on building accessible, human-centered products.
+      </motion.p>
+    </section>
+  )
+}
